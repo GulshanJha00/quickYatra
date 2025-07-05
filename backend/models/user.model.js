3f70fb435c7861c40a16afd57156ce1f26e00038
GulshanJha00 <gk4763549@gmail.com>
2025-07-06 01:06:16 +0530

    route model created

---
+++ backend/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+
+const userschema = new mongoose.Schema({
+    fullname:{
+        firstName:{
+            type: String,
+            required: true,
+            minglength: [3,"First name should be more than 3 character long"]
+        },
+        lastName:{
+            type: String,
+            minglength: [3,"Last name should be more than 3 character long"]
+        }
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [3,"Email should be more than 3 character long"],
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false, //when we find user password dont go to db
+    },
+    socketId:{
+        type: String
+    }
+})
+
+userschema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id},process.env.JWT_KEY)
+    return token;
+}
+userschema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+userschema.statics.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10)
+}
+
+module.exports = mongoose.model('user',userschema);
